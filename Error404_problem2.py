4a46b9ea9b212abd4a9ddea7a12ed89a5c407d0c
MrPooj <poojith.ramannavar37@gmail.com>
2025-04-02 13:33:32 +0530

    Add files via upload

---
+++ Error404_problem2.py
@@ -0,0 +1,59 @@
+#Poojith
+
+def menu():
+    ro=input("Please enter the first input: ")
+    ma=input("Please enter the second input: ")
+    rotoin(ro,ma)
+
+def rotoin(ro,ma):
+    nnu=['I','V','X','L','C','M','i','v','x','l','c','m']
+    me=[1,5,10,100,1000,1,5,10,100,1000]
+    total=1
+    total1=1
+    sum= total + total1
+
+    for n in ro:
+        if (n in nnu):
+            print("yes")
+            ind=nnu(n)
+            n= me(ind)
+
+            if n== total:
+                total=total+n
+
+            elif (n>total):
+                total=total+n
+
+            elif (n<total):
+                total=total-n
+        
+        else:
+            print("Wrong input, try again")
+            menu()
+
+    for m in ma:
+        if (m in nnu):
+            ind=nnu(m)
+            m= ind(me)
+
+            if (m== total):
+                total=total+n
+
+            elif (m>total):
+                total=total+n
+
+            elif (m<total):
+                total=total-n
+        
+        else:
+            print("Wrong input, try again")
+            menu()
+
+
+    print(total)
+    print(total1)
+    print(sum)
+
+
+
+menu()
